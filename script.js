986396e367fdca2fa73c2071099639e537eab0b7
Almoali <almoali399@outlook.com>
2024-02-01 04:05:37 +0200

    adding a menu bar  in index.html with javascript and renamed the javascript file from typesmt to script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.getElementById("searchForm").addEventListener("submit", function (event
 
         "./thepillars": ["pillar", "fasting", "charity", "pilgrimage", "belief", "divine", "faithful", "worshiper", "pious", "devout", "pillars"],
 
-        "./theka3ba.html": ["Kaaba", "Mecca", "pilgrimage", "hajj", "worship", "sacred", "spiritual", "devotion", "Muslims", "sacred site"],
+        "./theka3ba.html": ["kaaba", "Mecca", "pilgrimage", "hajj", "worship", "sacred", "spiritual", "devotion", "Muslims", "sacred site", "mecca"],
 
         "./aboutquran": ["quran", "revelation", "scripture", "recitation", "guidance", "inspiration", "divine", "truth", "spiritual"],
 
@@ -38,3 +38,35 @@ document.getElementById("searchForm").addEventListener("submit", function (event
         window.location.href = "./notfound";
     }
 });
+
+
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "100";
+    document.getElementById("main").style.marginLeft = "100";
+}
+
+function toggleNav() {
+    var sidenav = document.getElementById("mySidenav");
+    if (sidenav.style.height === "100%" || sidenav.style.height === "") {
+      sidenav.style.height = "0";
+    } else {
+      sidenav.style.height = "100%";
+    }
+  }
+
+
+// Function to close the side navigation menu when clicking outside of it
+function closeNavOutside(event) {
+    var sidenav = document.getElementById("mySidenav");
+    // Check if the clicked element is not inside the side navigation
+    if (!event.target.closest('.sidenav')) {
+        sidenav.style.height = "0";
+        document.body.removeEventListener('click', closeNavOutside);
+    }
+}
