31a14c87b0d9915055bc691d3da347229674559f
Almoali <almoali399@outlook.com>
2024-02-26 00:17:41 +0200

    adding js to every webpage and adding menu-bar to the remaining webpages as well and chneging ka3ba to kaaba

--- contact.js
+++ contact.js
@@ -37,3 +37,35 @@ function sendEmail() {
         message => alert(message)
     );
 }
+
+
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "100";
+    document.getElementById("main").style.marginLeft = "100";
+}
+
+function toggleNav() {
+    var sidenav = document.getElementById("mySidenav");
+    if (sidenav.style.height === "100%" || sidenav.style.height === "") {
+        sidenav.style.height = "0";
+    } else {
+        sidenav.style.height = "100%";
+    }
+}
+
+
+// Function to close the side navigation menu when clicking outside of it
+function closeNavOutside(event) {
+    var sidenav = document.getElementById("mySidenav");
+    // Check if the clicked element is not inside the side navigation
+    if (!event.target.closest('.sidenav')) {
+        sidenav.style.height = "0";
+        document.body.removeEventListener('click', closeNavOutside);
+    }
+}
