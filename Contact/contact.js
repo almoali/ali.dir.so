56bc176941a2fadd8fe44114c1df44c8a7772924
Almoali <almoali399@outlook.com>
2024-03-30 14:05:27 +0200

    Changing the date from march 11(ramadan) to aapril 10(eid)

--- Contact/contact.js
+++ Contact/contact.js
@@ -1,5 +1,5 @@
 function countdown() {
-    var targetDate = new Date("March 11, 2024 00:00:00").getTime();
+    var targetDate = new Date("April 10, 2024 00:00:00").getTime();
 
     var x = setInterval(function () {
         var now = new Date().getTime();
@@ -7,7 +7,7 @@ function countdown() {
 
         if (distance <= 0) {
             clearInterval(x);
-            document.getElementById("countdown").innerHTML = "Ramadan Mubarak";
+            document.getElementById("countdown").innerHTML = "Eid Mubarak";
         } else {
             var days = Math.floor(distance / (1000 * 60 * 60 * 24));
             var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
